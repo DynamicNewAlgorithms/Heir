9f0cd3e498e6eb9619f899862fa200a051e3fecf
BillCI <bill@concreteinteractive.com>
2014-11-29 12:54:53 -0800

    base stats

--- db/migrate/20141129203517_create_base_stats.rb
+++ db/migrate/20141129203517_create_base_stats.rb
@@ -1,7 +1,11 @@
 class CreateBaseStats < ActiveRecord::Migration
   def change
     create_table :base_stats do |t|
-
+      t.string :name
+      t.integer :vigor
+      t.integer :arcane
+      t.integer :cunning
+      t.belongs_to :card
       t.timestamps
     end
   end
