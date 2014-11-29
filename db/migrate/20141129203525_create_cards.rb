0a6f2cd695257cfc59f53e625bf7095e32a77dbf
XanderA <alexanderagosta@gmail.com>
2014-11-29 13:10:58 -0800

    create actins, cards, resources

--- db/migrate/20141129203525_create_cards.rb
+++ db/migrate/20141129203525_create_cards.rb
@@ -1,7 +1,9 @@
 class CreateCards < ActiveRecord::Migration
   def change
     create_table :cards do |t|
-
+      t.string :name
+      t.belongs_to :base_stat
+      t.belongs_to :hands
       t.timestamps
     end
   end
--- db/migrate/20141129203537_create_actions.rb
+++ db/migrate/20141129203537_create_actions.rb
@@ -1,7 +1,8 @@
 class CreateActions < ActiveRecord::Migration
   def change
     create_table :actions do |t|
-
+      t.belongs_to :card
+      t.string :name
       t.timestamps
     end
   end
