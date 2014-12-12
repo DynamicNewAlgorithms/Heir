6c28ba05aeaf23fa975d31b6fd5c0d9efe2c2051
BillCI <bill@concreteinteractive.com>
2014-12-12 13:44:07 -0800

    need to start thinking about how the js will build the world and play the game

--- app/models/code.rb
+++ app/models/code.rb
@@ -1,4 +1,21 @@
 class Code < ActiveRecord::Base
   validates :name,:kind, presence: true
   validates :kind
+
+  # methods needed to be valid
+  # .draw(x,y) -> will draw the element at the location
+  # .hit_box() -> will return the hit box for event interactions
+  # .mouse_enter -> event triggered when mouse enters this elements hit box
+  # .mouse_leave -> event
+  # .mouse_down -> event
+  # .mouse_up -> event
+  # .mouse_wheal -> event
+  # .key_down -> event
+  # .key_up -> event
+  # .new_turn -> turn phase one
+  # .locked_in -> event when users have locked in
+  # .flip -> event when all users have locked in
+  # .resolve -> event that resolves flipped cards
+  # TODO:iron out the phases of a turn
+  # .finish_turn -> event right before new_turn is called
 end
